a5a994cd5925d72d240c8b0cdd7664327c6ad72c
Marcel <marcel@example.com>
2024-09-25 06:20:50 +0000

    hola usa la clase HolaMundo

---
+++ hola.php
@@ -0,0 +1,8 @@
+<?php
+// Autor: Sergio Gómez <sergio@uco.es>
+// El nombre por defecto es Mundo
+require('HolaMundo.php');
+
+$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
+print new HolaMundo($nombre);
+?>
